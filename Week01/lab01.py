b5983d8ad4724cc79239923651600b3fffc19cdb
diana mohammadi <dianamohammadi@dianas-MacBook-Pro.local>
2025-01-12 11:49:18 -0500

    week1 lab answers

--- Week01/lab01.py
+++ Week01/lab01.py
@@ -1,1 +1,23 @@
-print("hello word.")
+#Sample Coding Questions 01 Week
+
+#1)
+from array import *
+number = array("i", [1, 4, 7, 9])
+
+#2)
+a = 1
+b=2
+c=3
+d=4
+e = a-((b**c)//d)+(a%c)
+
+#3)
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+#4)
+userAge = int(input("Please enter your age: "))
+userAge += 22
+print(f"Now showing the shop items filtered by age: {userAge}")
+
+
